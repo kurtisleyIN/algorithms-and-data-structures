cba4d033a675298d13cc1e20191944c19c3e7ae7
kurtisleyIN <56206183+kurtisleyIN@users.noreply.github.com>
2020-05-05 19:31:31 -0400

    Create max_pairwise_product.py

---
+++ Algorithmic Design and Techniques/max_pairwise_product.py
@@ -0,0 +1,21 @@
+# python3
+n = int(input())
+a = [int(x) for x in input().split()]
+assert(len(a) == n)
+
+Index1 = 0
+for i in range(1,n):
+    if a[i] > a[Index1]:
+        Index1 = i
+        
+if Index1 == 0:
+    Index2 = 1
+else:
+    Index2 = 0
+
+for i in range(1,n):
+    if i != Index1 and a[i] > a[Index2]:
+        Index2 = i
+        
+result = a[Index1]*a[Index2]
+print(result)
