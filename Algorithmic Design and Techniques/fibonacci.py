51e149390a29e0e25e2630ba031ee3df7c70f537
kurtisleyIN <56206183+kurtisleyIN@users.noreply.github.com>
2020-05-04 22:13:36 -0400

    Create fibonacci.py

---
+++ Algorithmic Design and Techniques/fibonacci.py
@@ -0,0 +1,13 @@
+# python3
+def calc_fib(n):
+    Fib = [0,1]
+    
+    if n <= 1:
+        return n
+    for i in range(2,n+1):
+        Fib.append(Fib[i-1] + Fib[i-2])
+
+    return Fib[n]
+
+n = int(input())
+print(calc_fib(n))
