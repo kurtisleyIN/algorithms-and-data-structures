f9a2fd8b78309da724a83615de340a02df736fb3
Kurtis Ley <kurtis_ley@sweetwater.com>
2022-02-12 20:34:18 -0500

    restructure functions to actually output values, print within global scope

--- algs200x-algorithmic-design-and-techniques/pa1-programming-challenges/1-1-APlusB.py
+++ algs200x-algorithmic-design-and-techniques/pa1-programming-challenges/1-1-APlusB.py
@@ -4,10 +4,10 @@ import sys
 
 def main(numbers: list) -> None:
     """ Print the sum of the first two integers in a list """
-    print(int(numbers[0]) + int(numbers[1]))
+    return int(numbers[0]) + int(numbers[1])
 
 
 if __name__ == '__main__':
 
     tokens = sys.stdin.read().split()
-    main(tokens)
+    print(main(tokens))
--- algs200x-algorithmic-design-and-techniques/pa1-programming-challenges/1-2-max-pairwise-product.py
+++ algs200x-algorithmic-design-and-techniques/pa1-programming-challenges/1-2-max-pairwise-product.py
@@ -6,7 +6,8 @@ def main(numbers):
     largest_number = max(numbers)
     numbers.remove(largest_number)
     next_largest_number = max(numbers)
-    print(largest_number*next_largest_number)
+
+    return largest_number*next_largest_number
 
 
 if __name__ == '__main__':
@@ -15,4 +16,4 @@ if __name__ == '__main__':
     sequence = [int(x) for x in input().split()]    # Sequence of integers
     assert(n == len(sequence))
 
-    main(sequence)
+    print(main(sequence))
